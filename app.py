19191995b4f2859f2f863d1b8220442eb06cdfcb
BakungaBronson <bakungabro@gmail.com>
2022-07-20 09:42:45 +0300

    Added authentication

--- app.py
+++ app.py
@@ -3,44 +3,83 @@ import cv2
 # Import matplotlib so we can visualize an image
 from matplotlib import pyplot as plt
 
-# Connect to capture device
-cap = cv2.VideoCapture(3)
+import tkinter as tk
+from tkinter import *
+from PIL import ImageTk, Image
+import os
+import requests
+from requests.structures import CaseInsensitiveDict
+import json
+import threading
+import datetime
+import imutils
 
-# Get a frame from the capture device
-ret, frame = cap.read()
+root = tk.Tk()
+root.title("PhotoBooth App")
+root.geometry('600x500')
 
-print(ret)
+def authenticate():
+    user = username.get("1.0", END)
+    passwrd = password.get("1.0", END)
 
-print(frame)
+    print(user, passwrd)
 
-plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-plt.show()
+    url = "https://muele.mak.ac.ug/login/token.php?service=moodle_mobile_app"
 
-# Releases capture back into the wild 
-cap.release()
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/x-www-form-urlencoded"
 
-def take_photo(): 
-    cap = cv2.VideoCapture(3)
-    ret, frame = cap.read()
-    cv2.imwrite('webcamphoto.jpg', frame)
-    cap.release()
+    data = "username={0}&password={1}".format(user.strip(), passwrd.strip())
 
-# Connect to webcam
-cap = cv2.VideoCapture(3)
-# Loop through every frame until we close our webcam
-while cap.isOpened(): 
-    ret, frame = cap.read()
-    
-    # Show image 
-    cv2.imshow('Webcam', frame)
-    
-    # Checks whether q has been hit and stops the loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    elif cv2.waitKey(1) & 0xFF == ord('t'):
-        take_photo()
+    resp = requests.post(url, headers=headers, data=data)
+    resp_json = resp.json()
 
-# Releases the webcam
-cap.release()
-# Closes the frame
-cv2.destroyAllWindows()
+    try:
+        if resp_json['token']:
+            output.insert(END, "Successfully logged in as {}".format(user))
+
+            def take_photo(): 
+                cap = cv2.VideoCapture(0)
+                ret, frame = cap.read()
+                cv2.imwrite('webcamphoto.jpg', frame)
+                cap.release()
+
+            # Connect to webcam
+            cap = cv2.VideoCapture(0)
+            # Loop through every frame until we close our webcam
+            while cap.isOpened(): 
+                ret, frame = cap.read()
+                
+                # Show image 
+                cv2.imshow('Webcam', frame)
+                
+                # Checks whether q has been hit and stops the loop
+                if cv2.waitKey(1) & 0xFF == ord('t'):
+                    take_photo()
+
+            # Releases the webcam
+            cap.release()
+            # Closes the frame
+            cv2.destroyAllWindows()
+    except:
+        output.insert(END, "Something went wrong. Try again.")
+
+img = ImageTk.PhotoImage(Image.open("images.png"))
+panel = Label(root, image = img)
+panel.pack()
+
+username = Text(root, height =  2, width = 52)
+username.insert(END, 'Username')
+username.pack()
+
+password = Text(root, height =  2, width = 52)
+password.insert(END, 'Password')
+password.pack()
+
+convert_button = Button(root, height = 2, width = 20, text = "Submit", command = authenticate)
+convert_button.pack()
+
+output = Text(root, height = 5, width = 52, bg = "light blue")
+output.pack()
+
+root.mainloop()
